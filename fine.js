0ed8dc6e00efd1d43de51c86396cb60bbd0996f0
Shozaib-Khan <70322458+Shozaib-Khan@users.noreply.github.com>
2024-06-07 17:54:06 +0530

    Update - form link to sheets

--- fine.js
+++ fine.js
@@ -11,3 +11,17 @@ document.getElementById('copy-email').addEventListener('click', function(event) 
 
   alert('Email ID copied to clipboard!');
 });
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzBkY2Zq5Hcwd6BU2P6ZUuBy8VnMqL4ioed1aokUzzYVI-okXHyneRZxHbj2meNqAQgeg/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+ e.preventDefault()
+ fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+ .then(response => alert("Thank you! your form is submitted successfully." ))
+ .then(() => { window.location.reload(); })
+ .catch(error => console.error('Error!', error.message))
+})
